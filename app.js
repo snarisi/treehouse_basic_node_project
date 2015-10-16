1310edd2e2bf7318bb5af8549a3efc8ef621b061
Sam Narisi <snarisi@gmail.com>
2015-10-15 20:07:39 -0400

    Add eror handling

--- app.js
+++ app.js
@@ -6,14 +6,24 @@ var http = require('http');
 var username = 'samuelnarisi';
 
 var request = http.get("http://teamtreehouse.com/" + username + ".json", function(response) {
-  console.log(response.statusCode);
   var body = "";
+  
   response.on('data', function(chunk) {
     body += chunk;
   });
+  
   response.on('end', function() {
-    body = JSON.parse(body);
-    printData(body.name, body.badges.length, body.points.JavaScript);
+    if (response.statusCode === 200) {
+      try {
+        body = JSON.parse(body);
+        printData(body.name, body.badges.length, body.points.JavaScript);
+      } catch(error) {
+        //TODO: make erorr more user friendly
+        printError(error);
+      }
+    } else {
+      printError({message: "Error getting profile for " + username + ". (" + http.STATUS_CODES[response.statusCode] + ")"});
+    }
   });
 });
 
@@ -21,3 +31,10 @@ function printData(user, badges, points) {
   var message = user + " has " + badges + " total badge(s) and " + points + " points in JavaScript.";
   console.log(message);
 }
+
+//print errors
+function printError(error) {
+  console.error(error.message);
+}
+
+request.on('error', printError);
