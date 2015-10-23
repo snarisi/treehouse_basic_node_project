5ea8568d7ec0c5e5a9cadae0fa7914d159b7aea3
Sam Narisi <snarisi@gmail.com>
2015-10-23 13:11:27 -0400

    Accept topic area as command line argument

--- app.js
+++ app.js
@@ -1,10 +1,7 @@
 var profiles = require('./profiles');
-//profiles.get(process.argv[2]);
 
 var users = process.argv.slice(3);
 var topic = process.argv[2];
 users.forEach(function(user) {
   profiles.get(user, topic);
 });
-
-//TODO: put all printer functionality into its own module
