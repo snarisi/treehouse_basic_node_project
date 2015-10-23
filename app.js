a8a5e2f10e80fed4b466917fb2dae5a3fc606db8
Sam Narisi <snarisi@gmail.com>
2015-10-23 13:34:56 -0400

    Accept topic area as command line argument

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 var profiles = require('./profiles');
 
-var users = process.argv.slice(3);
+//accepts topic area as first command line argument
 var topic = process.argv[2];
+
+//accepts variable number of users as second/third/etc CL args
+var users = process.argv.slice(3);
+
 users.forEach(function(user) {
   profiles.get(user, topic);
 });
